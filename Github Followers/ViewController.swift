ced19f14745b9f888b477e6d62b84df38e358ec2
Vivek Rai <vivekraiiosdev@gmail.com>
2020-01-19 16:19:24 +0530

    Initial Commit

---
+++ Github Followers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github Followers
+//
+//  Created by Vivek Rai on 19/01/20.
+//  Copyright © 2020 Vivek Rai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
